0a564dfd13df61f86c83412d3494462800d242d1
Tharusha Nimsara <c.t.nimsara@gmail.com>
2024-07-14 19:58:41 +0530

    0.2.1

--- Recipe/Models/RecipeDBContext.cs
+++ Recipe/Models/RecipeDBContext.cs
@@ -13,7 +13,7 @@ namespace Recipe.Models
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connectionString = @"Server=localhost;Database=RecipeDB;Trusted_Connection=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connectionString = @"Server=ccpa7stkruda3o.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com;port=5432;Database=d6hv6qpoffl4i;User ID=uc3batrenl2oje;Password=p84188410d7e7f27a02905db276f210ff1c3d4e429840a11a0f6a47b9f200624d;Trusted_Connection=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             optionsBuilder.UseSqlServer(connectionString);
         }
     }
